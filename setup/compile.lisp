3a3812c0fa5313750fee5f10572b62208d437677
Ben Hyde <bhyde@pobox.com>
2013-04-12 22:03:45 -0400

    Revise to my taste.

--- setup/compile.lisp
+++ setup/compile.lisp
@@ -1,57 +1,56 @@
 (in-package :cl-user)
 
-(defvar *build-dir* (pathname-directory (pathname (concatenate 'string (getenv "BUILD_DIR") "/"))))
-(defvar *cache-dir* (pathname-directory (pathname (concatenate 'string (getenv "CACHE_DIR") "/"))))
-(defvar *buildpack-dir* (pathname-directory (pathname (concatenate 'string (getenv "BUILDPACK_DIR") "/"))))
+(flet ((env-to-dirs (x)
+         (pathname-directory (pathname (concatenate 'string (getenv x) "/")))))
+  (defvar *build-dir* (env-to-dirs "BUILD_DIR"))
+  (defvar *cache-dir* (env-to-dirs "CACHE_DIR"))
+  (defvar *buildpack-dir* (env-to-dirs "BUILDPACK_DIR")))
 
-(format t "~%*build-dir* = ~a" *build-dir*)
-(format t "~%*cache-dir* = ~a" *cache-dir*)
-(format t "~%*buildpack-dir* = ~a" *buildpack-dir*)
+(format t "~&*build-dir* = ~a" *build-dir*)
+(format t "~&*cache-dir* = ~a" *cache-dir*)
+(format t "~&*buildpack-dir* = ~a" *buildpack-dir*)
 
-;;; Tell ASDF to store binaries in the cache dir
+;;; Tell ASDF to store binaries in the cache dir.
 (ccl:setenv "XDG_CACHE_HOME" (concatenate 'string (getenv "CACHE_DIR") "/.asdf/"))
 
-(require :asdf)
+; (require :asdf) ;; we want quicklisp to control which asdf we get.
 
+;;; Setup quicklisp (i.e. what is usually done in a user's init file).
 (let ((ql-setup (make-pathname :directory (append *cache-dir* '("quicklisp")) :defaults "setup.lisp")))
-  (if (probe-file ql-setup)
-      (load ql-setup)
-      (progn
-	(load (make-pathname :directory (append *buildpack-dir* '("lib")) :defaults "quicklisp.lisp"))
-	(funcall (symbol-function (find-symbol "INSTALL" (find-package "QUICKLISP-QUICKSTART")))
-		 :path (make-pathname :directory (pathname-directory ql-setup))))))
 
-;;; Load all .asd files in the repos subdirectory.  The compile script puts
-;;; several systems in there, because we are using versions that are 
-;;; different from those in Quicklisp. (update: Can't just load the files apparently,
-;;; have to add dirs to asdf:*central-registry*.  Blah.
-(let* ((asds (directory (make-pathname :directory  (append *cache-dir* '( "repos" :wild-inferiors))
-				       :name :wild
-				       :type "asd")))
-       (directories (remove-duplicates (mapcar #'pathname-directory asds) :test #'equal)))
-  (dolist (d directories)
-    (format t "~%~a added to asdf registry" d)
-    (push (make-pathname :directory d) asdf:*central-registry*)))
+  ;; If this is a fresh cache we need to install quicklisp first.
+  (unless (probe-file ql-setup)
+    (load (make-pathname :directory (append *buildpack-dir* '("lib")) :defaults "quicklisp.lisp"))
+    (eval (read-from-string (format nil "(quicklisp-quickstart:install :path ~S)" 
+                                    (make-pathname :directory (pathname-directory ql-setup))))))
+
+  (load ql-setup))
+
+;;; Notify ASDF that our build and cache dir is an awesomes place to find asf files.
+(initialize-source-registry `(:source-registry
+                              (:tree ,(make-pathname :directory *buildpack-dir*))
+                              (:tree ,(make-pathname :directory *cache-dir*))
+                              :default-registry))
 
 ;;; App can redefine this to do runtime initializations
 (defun initialize-application (&key port)
-  )
-
-(defvar *root* "/app")			;this is always the app root on Heroku now?
+  (declare (ignore port)))
 
 ;;; Default toplevel, app can redefine.
 (defun heroku-toplevel ()
   (initialize-application :port (parse-integer (getenv "PORT")))
-  (loop (sleep 60))			;sleep forever
-  )
+  (loop (sleep 600)))			;sleep forever
 
-;;; Load the application from sources
+(defvar *root* "/app")			;this is always the app root on Heroku now?
+
+;;; Run the app's own build.
+(format t "~2&* Load application's heroku-compile.lisp " app-file)
 (load (make-pathname :directory *build-dir* :defaults "heroku-compile.lisp"))
 
 ;;; Save the application as an image
-(let ((app-file (format nil "~A/lispapp" (getenv "BUILD_DIR")))) ;must match path specified in bin/release
-  (format t "Saving to ~A~%" app-file)
+(let ((app-file (make-pathname :directory *build-dir* :defaults "lispapp")))
+  ;; note that the buildpack's bin/release refers to this application name.
+  (format t "~2&* save-application to create slug's lispapp, i.e. ~A" app-file)
   (save-application app-file
 		    :prepend-kernel t
-		    :toplevel-function #'heroku-toplevel
-		    ))
+		    :toplevel-function #'heroku-toplevel))
