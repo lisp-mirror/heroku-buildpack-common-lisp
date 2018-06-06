b4e19bd69434ca838c731f2138ed254678044916
Duncan Bayne <duncan@bayne.id.au>
2018-06-07 08:15:18 +1000

    Still trying to figure out directory structure under test

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -3,9 +3,9 @@
 . "${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh"
 
 testCompile() {
-  pwd
-  ls -l
-  cp -R "fixtures/ccl/*" "${BUILD_DIR}"
+  ls -lR "$BUILDPACK_HOME}"
+  cp -R "${BUILDPACK_HOME}/fixtures/ccl/*" "${BUILD_DIR}"
+
   compile
   assertCapturedSuccess
 }
