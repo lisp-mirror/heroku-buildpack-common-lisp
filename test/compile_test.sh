ae73b320c608c8e30c897df796811815eb7a4390
Duncan Bayne <duncan@bayne.id.au>
2018-06-07 08:22:50 +1000

    Remove debug logging

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -3,9 +3,7 @@
 . "${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh"
 
 testCompile() {
-  ls -lR "$BUILDPACK_HOME"
   cp -R "${BUILDPACK_HOME}/test/fixtures/ccl/*" "${BUILD_DIR}"
-
   compile
   assertCapturedSuccess
 }
