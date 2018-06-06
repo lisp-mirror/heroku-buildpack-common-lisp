5e233ea6f044b1c935067a35077b88f0426ea74a
Duncan Bayne <duncan@bayne.id.au>
2018-06-07 08:48:44 +1000

    Remove possibly unused QL system

--- test/fixtures/ccl/heroku-compile.lisp
+++ test/fixtures/ccl/heroku-compile.lisp
@@ -2,4 +2,5 @@
 
 (in-package :cl-user)
 
-(ql:quickload :superman) ;; load this, compiling if necessary.
+;; TODO: duncan@bayne.id.au: why was this loaded?
+;; (ql:quickload :superman) ;; load this, compiling if necessary.
