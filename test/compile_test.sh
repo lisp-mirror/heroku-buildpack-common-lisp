cf5eea770311f78cf26dd1fd772f8909a648f229
Duncan Bayne <duncan@bayne.id.au>
2018-06-07 08:01:13 +1000

    Move fixtures to (hopefully) known location

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -3,15 +3,8 @@
 . "${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh"
 
 testCompile() {
-  echo "BUILD_DIR: $BUILD_DIR"
-  echo "CACHE_DIR: $BUILD_DIR"
-  echo "ENV_DIR: $ENV_DIR"
-  echo "CL_IMPL: $CL_IMPL"
-
-  echo "ls -l ${BUILD_DIR}"
-  ls -l "${BUILD_DIR}"
-
-  cp -R "${BUILDPACK_HOME}/../fixtures/ccl/*" "${BUILD_DIR}"
+  SRC_DIR=dirname "$0"
+  cp -R "$SRC_DIR/fixtures/ccl/*" "${BUILD_DIR}"
   compile
   assertCapturedSuccess
 }
