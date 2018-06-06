18e7c06f3bfd2ce0f49d309eb5a07f4bd17b0dfe
Duncan Bayne <duncan@bayne.id.au>
2018-06-07 08:40:57 +1000

    Revert "Extract fixture copy into test setup function"
    This reverts commit 11dd35f813f274ef4c47936ba7df29435f5c81df.

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -2,11 +2,11 @@
 
 . "${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh"
 
-setUp() {
-  cp -Rv "${BUILDPACK_HOME}"/test/fixtures/ccl/* "${BUILD_DIR}"
-}
-
 testCompile() {
+  cp -Rv "${BUILDPACK_HOME}"/test/fixtures/ccl/* "${BUILD_DIR}"
+  echo "BUILD_DIR: $BUILD_DIR"
+  ls -lR "${BUILD_DIR}"
+
   compile
   assertCapturedSuccess
 }
