c0ade1fa85f520bd705bf23a7c941870ced9e9d3
Duncan Bayne <duncan@bayne.id.au>
2018-06-07 08:30:35 +1000

    It was copying into the correct location, but possibly nothing

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -3,10 +3,7 @@
 . "${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh"
 
 testCompile() {
-  cp -R "${BUILDPACK_HOME}/test/fixtures/ccl/*" "${BUILD_DIR}"
-
-  set
-
+  cp -Rv "${BUILDPACK_HOME}"/test/fixtures/ccl/* "${BUILD_DIR}"
   echo "BUILD_DIR: $BUILD_DIR"
   ls -lR "${BUILD_DIR}"
 
