5de2e55145be356b1acdc52f63ad9d4e3a9784b8
Duncan Bayne <duncan@bayne.id.au>
2018-06-07 07:52:10 +1000

    Add some debugging logs to track down compilation failure

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -3,6 +3,14 @@
 . "${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh"
 
 testCompile() {
+  echo "BUILD_DIR: $BUILD_DIR"
+  echo "CACHE_DIR: $BUILD_DIR"
+  echo "ENV_DIR: $ENV_DIR"
+  echo "CL_IMPL: $CL_IMPL"
+
+  echo "ls -l ${BUILD_DIR}"
+  ls -l "${BUILD_DIR}"
+
   cp -R "${BUILDPACK_HOME}/fixtures/ccl/*" "${BUILD_DIR}"
   compile
   assertCapturedSuccess
