63281993f0bda892ee11df297a7751e17daa9f47
Duncan Bayne <duncan@bayne.id.au>
2018-06-07 08:03:44 +1000

    Add further logging to see where the test script is called from

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -3,8 +3,9 @@
 . "${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh"
 
 testCompile() {
-  SRC_DIR=dirname "$0"
-  cp -R "$SRC_DIR/fixtures/ccl/*" "${BUILD_DIR}"
+  pwd
+  ls -l
+  cp -R "fixtures/ccl/*" "${BUILD_DIR}"
   compile
   assertCapturedSuccess
 }
