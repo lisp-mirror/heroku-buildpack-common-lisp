a1ddb2dc528887d81c6bf08f8654ce160fbb81b7
Duncan Bayne <duncan@bayne.id.au>
2018-06-07 08:18:19 +1000

    Fix typo in logging

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -3,7 +3,7 @@
 . "${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh"
 
 testCompile() {
-  ls -lR "$BUILDPACK_HOME}"
+  ls -lR "$BUILDPACK_HOME"
   cp -R "${BUILDPACK_HOME}/fixtures/ccl/*" "${BUILD_DIR}"
 
   compile
